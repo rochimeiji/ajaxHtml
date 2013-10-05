45bd5fff43299e05293d51a77b55d2858409e144
rochimeiji <rochim.eiji@yahoo.com>
2013-10-05 13:44:17 +0700

    Coment di home controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,6 +4,7 @@ class home extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();	
+		//zainu rochim
 		//$this->session->sess_destroy();
 		$this->load->library('pagination');
 	}
