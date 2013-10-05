f113bb2c881d38cdca1d9a720d8959be77ddeb96
rochimeiji <rochim.eiji@yahoo.com>
2013-10-05 18:28:37 +0700

    Hello

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
  * This can be set to anything, but default usage is:
  *
  *     development
- *     testing
+ *     testing rochim
  *     production
  *
  * NOTE: If you change these, also change the error_reporting() code below
